3c66e3b7dcedec9b752646af6e0d26a105f2f247
Cesar Rios <cesar.cr7.rios@gmail.com>
2018-07-24 16:48:02 -0500

    Admin Completo

--- src/CoreBundle/Form/AdministradoresType.php
+++ src/CoreBundle/Form/AdministradoresType.php
@@ -3,7 +3,6 @@
 namespace CoreBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
